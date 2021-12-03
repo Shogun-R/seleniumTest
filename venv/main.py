932d349905d27e961ab7cab9eca3a5f847fad0b6
Shogun-R <58804006+Shogun-R@users.noreply.github.com>
2021-12-04 00:36:07 +0100

    still trying to get the damn page to change, probably gonna use pyautogui

--- venv/main.py
+++ venv/main.py
@@ -1,17 +1,31 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 PATH = 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'
+debug = 10
+debug0 = debug + 5
+debug1 = debug0 + 5
+
 
 browser = webdriver.Chrome(PATH)
 try:
     browser.get('https://typistapp.ca/#/home')
-    browser.implicitly_wait(3000)
+    browser.implicitly_wait(30)
     browser.maximize_window()
     browser.find_element_by_id('usernameInput').send_keys('Altair4132')
     browser.find_element_by_id('passwordInput').send_keys('17082002sv')
     browser.find_element_by_class_name('submit').click()
+    time.sleep(15)
+    print(debug)
+    WebDriverWait(webdriver, 10).until(EC.element_to_be_clickable('//a[contains(@href,"#/level/1"]'))
+    print(debug)
+    browser.find_element_by_xpath('//a[contains(@href,"#/level/1"]').click()
+    print(debug)
+
 
 except NoSuchElementException:
     pass
